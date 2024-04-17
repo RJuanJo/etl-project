20243d0d5a475ab376276776c5bb27c1b2e0d830
JuancaBurbano <juan_camilo.burbano@uao.edu.co>
2024-04-16 23:52:19 -0500

    Api

---
+++ Api/ApiFusion.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+def combine_datasets(dataset1, dataset2, dataset3):
+    # List of columns we want to keep in the combination
+    merge_columns = ['url', 'id', 'name', 'stars', 'numberOfGuests', 'address', 'roomType',
+                     'location', 'reviews', 'pricing', 'isAvailable', 'photos', 'primaryHost',
+                     'additionalHosts', 'isHostedBySuperhost', 'calendar', 'occupancyPercentage']
+    merged_df = pd.merge(dataset1, dataset2, on=merge_columns, how='outer')
+    merged_df = pd.merge(merged_df, dataset3, on=merge_columns, how='outer')
+    merged_df.to_csv('dataApi.csv', index=False)
+    print("Shape of the resulting DataFrame:", merged_df.shape)
+    return merged_df
+
+dataset1 = pd.read_csv('dataBl.csv')
+dataset2 = pd.read_csv('dataBC.csv')
+dataset3 = pd.read_csv('dataJT.csv')
+
+combine_datasets(dataset1, dataset2, dataset3)
+ 
